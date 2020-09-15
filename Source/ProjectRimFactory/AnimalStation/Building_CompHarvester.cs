cb46c207e994d18df41e2976652d940a5c5df6ab
HR_SP <HR_SP@github>
2020-09-15 02:28:50 +0200

    dynamic power for animal harvester

--- Source/ProjectRimFactory/AnimalStation/Building_CompHarvester.cs
+++ Source/ProjectRimFactory/AnimalStation/Building_CompHarvester.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 using System.Reflection;
 using System.Linq;
 using ProjectRimFactory.Common;
+using ProjectRimFactory.SAL3.Tools;
+using System;
 
 namespace ProjectRimFactory.AnimalStation
 {
-    public abstract class Building_CompHarvester : Building_Storage , IPowerSupplyMachineHolder
+    public abstract class Building_CompHarvester : Building_Storage, IPowerSupplyMachineHolder
     {
         public static readonly PropertyInfo ResourceAmount = typeof(CompHasGatherableBodyResource).GetProperty("ResourceAmount", BindingFlags.NonPublic | BindingFlags.Instance);
         public static readonly PropertyInfo ResourceDef = typeof(CompHasGatherableBodyResource).GetProperty("ResourceDef", BindingFlags.NonPublic | BindingFlags.Instance);
@@ -24,12 +26,26 @@ namespace ProjectRimFactory.AnimalStation
 
         public IPowerSupplyMachine RangePowerSupplyMachine => this.GetComp<CompPowerWorkSetting>();
 
+        protected float defaultPower;
+
         public abstract bool CompValidator(CompHasGatherableBodyResource comp);
 
         public override void TickRare()
         {
             base.TickRare();
             if (!GetComp<CompPowerTrader>().PowerOn) return;
+
+            CompPowerTrader powerTrader = this.TryGetComp<CompPowerTrader>();
+            try
+            {
+                powerTrader.PowerOutput = BasePowerUtil.scanningPower;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                Log.Error(e.StackTrace);
+            }
+
             foreach (Pawn p in (from c in ScannerCells
                                 from p in c.GetThingList(Map).OfType<Pawn>()
                                 where p.Faction == Faction.OfPlayer
@@ -39,6 +55,16 @@ namespace ProjectRimFactory.AnimalStation
                                                                 where CompValidator(comp)
                                                                 select comp).ToList())
                 {
+                    try
+                    {
+                        powerTrader.PowerOutput = defaultPower;
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Error(e.Message);
+                        Log.Error(e.StackTrace);
+                    }
+
                     int amount = GenMath.RoundRandom((int)ResourceAmount.GetValue(comp, null) * comp.Fullness);
                     if (amount != 0)
                     {
@@ -57,6 +83,24 @@ namespace ProjectRimFactory.AnimalStation
                     }
                 }
             }
+
+            try
+            {
+                powerTrader.PowerOutput = BasePowerUtil.idlePower;
+            }
+            catch (Exception e)
+            {
+                Log.Error(e.Message);
+                Log.Error(e.StackTrace);
+            }
+        }
+
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+
+            var defPower = BasePowerUtil.getDefaultPower(this);
         }
     }
 }
